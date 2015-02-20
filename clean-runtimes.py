a1cdd0aca4deafddd5473bb680be7acb745ee876
Troy Dai <troy.dai@outlook.com>
2015-02-19 22:53:22 -0800

    add clean-runtime.py

---
+++ clean-runtimes.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python3
+
+# clean up all the runtime folders
+
+import os
+import os.path
+import shutil
+
+runtime_root = os.path.join(os.environ['USERPROFILE'], '.k', 'runtimes')
+runtime_folders = os.listdir(runtime_root)
+
+for r in runtime_folders:
+    print('removing', r)
+    shutil.rmtree(os.path.join(runtime_root, r))
+
