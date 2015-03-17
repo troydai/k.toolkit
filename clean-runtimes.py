17e80e9f2d24644d739f7731a53ff634490a9357
Troy Dai <troy.dai@outlook.com>
2015-03-17 11:27:30 -0700

    Update

--- clean-runtimes.py
+++ clean-runtimes.py
@@ -6,7 +6,7 @@ import os
 import os.path
 import shutil
 
-runtime_root = os.path.join(os.environ['HOME'], '.k', 'runtimes')
+runtime_root = os.path.join(os.environ['HOME'], '.dnx', 'runtimes')
 runtime_folders = os.listdir(runtime_root)
 
 for r in runtime_folders:
