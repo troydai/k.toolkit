5209b2020b1b746dac89878182d6e885bdb7960c
Troy Dai <troy.dai@outlook.com>
2015-03-31 22:25:11 -0700

    Clean packages on Linux

--- clean-packages.py
+++ clean-packages.py
@@ -6,6 +6,7 @@ import os
 import os.path
 import shutil
 
+# removing packages
 if os.name == 'nt':
     packages_root = os.path.join(os.environ['HOMEDRIVE'], os.environ['HOMEPATH'], '.dnx', 'packages')
 elif os.name == 'posix':
@@ -17,12 +18,18 @@ for p in packages_folder:
     print('removing', p)
     shutil.rmtree(os.path.join(packages_root, p))
 
-kpm_cache = os.path.join(os.environ['LocalAppData'], 'kpm', 'cache')
-if os.path.exists(kpm_cache):
-    print('removing', kpm_cache)
-    shutil.rmtree(kpm_cache)
+# clean kpm/dnu caches
+if os.name == 'nt':
+    local_app_data = os.environ['LocalAppData']
+elif os.name == 'posix':
+    local_app_data = '~/.local/share/'
 
-nuget_cache = os.path.join(os.environ['LocalAppData'], 'NuGet', 'Cache')
+dnx_cache = os.path.join(local_app_data, 'kpm', 'cache')
+if os.path.exists(dnx_cache):
+    print('removing', dnx_cache)
+    shutil.rmtree(dnx_cache)
+
+nuget_cache = os.path.join(local_app_data, 'NuGet', 'Cache')
 if os.path.exists(nuget_cache):
     print('removing', nuget_cache)
     shutil.rmtree(nuget_cache)
